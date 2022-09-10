4db89e110d50bf240c2edd5fc5d0a34b014be519
yashkhare11 <yash.khare2525@gmail.com>
2022-09-10 12:55:13 +0530

    Mathematical Operators

---
+++ Mathematical_Operators/Question1.js
@@ -0,0 +1,14 @@
+//# Problem 1 : Find Product
+
+//### Description
+
+//- You have to take given five numbers stored in variables with the following names
+    
+//    ```    one, two, three, four, five
+    
+//- Find the product of the five values, and print it
+
+
+// Method (hardcode)
+
+console.log(1*2*3*4*5)
---
+++ Mathematical_Operators/Question3.js
@@ -0,0 +1,12 @@
+// Problem 3 : Infinity Stones
+
+
+
+var one = 1, two = 2, three = 3, four = 4, five = 5, six = 6
+
+
+var double = two*2 + five*2 + six*2
+
+var thrice = three*3 + four*3
+
+console.log(double+thrice+one)
